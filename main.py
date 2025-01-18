9f196faf173dd6eda25db186302b48984dd354de
rnthking <rnthking@gmail.com>
2025-01-18 16:59:56 +0800

    new v2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World, bigluo good Bigluo tt kk"}
+    return {"message": "Hello World, bigluo good Bigluo tt kk change"}
 
 if __name__ == "__main__":
     import uvicorn
